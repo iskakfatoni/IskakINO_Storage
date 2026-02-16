ff2ae25394e6ad30d6bd1ec9074a05abf8953735
Iskak Fatoni <iskakfatoni@gmail.com>
2026-02-16 17:30:36 +0700

    Create IskakINO_Storage.cpp

---
+++ src/IskakINO_Storage.cpp
@@ -0,0 +1,16 @@
+/*
+ * src/IskakINO_Storage.cpp
+ * Implementasi instance global untuk IskakINO_Storage.
+ * Digunakan untuk mengalokasikan memori objek IskakStorage agar dapat diakses 
+ * di seluruh file project tanpa konflik definisi.
+ * * Created for: iskakfatoni (2026-02-16)
+ */
+
+#include "IskakINO_Storage.h"
+
+/**
+ * Inisialisasi objek global IskakStorage.
+ * User hanya perlu memanggil IskakStorage.begin() di setup() 
+ * tanpa perlu membuat objek baru.
+ */
+IskakINO_Storage IskakStorage;
