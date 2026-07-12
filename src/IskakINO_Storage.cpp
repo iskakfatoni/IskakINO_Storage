d963f255a8577741ffcc18465b89874526d1e3b2
Iskak Fatoni <iskakfatoni@gmail.com>
2026-07-12 10:57:58 +0700

    Fix undefined instance of IskakStorage
    Defined the global instance of IskakStorage to resolve linking issues in README examples.

--- src/IskakINO_Storage.cpp
+++ src/IskakINO_Storage.cpp
@@ -3,7 +3,15 @@
  * Implementasi instance global untuk IskakINO_Storage.
  * Digunakan untuk mengalokasikan memori objek IskakStorage agar dapat diakses 
  * di seluruh file project tanpa konflik definisi.
- * * Created for: iskakfatoni (2026-02-16)
+ *
+ * Fixed: instance IskakStorage sebelumnya tidak pernah didefinisikan,
+ * sehingga contoh penggunaan di README (IskakStorage.begin(...)) gagal
+ * di-link. File ini sekarang benar-benar mendefinisikan objeknya, sesuai
+ * dengan deklarasi `extern IskakINO_Storage IskakStorage;` di header.
+ *
+ * Created for: iskakfatoni (2026-02-16)
  */
 
 #include "IskakINO_Storage.h"
+
+IskakINO_Storage IskakStorage;
